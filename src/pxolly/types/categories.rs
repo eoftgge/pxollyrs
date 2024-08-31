cf1f584a3097f75bc43b22c79d71d8e5e1180578
consteremnaresinf <sandaar.ykt@gmail.com>
2024-08-31 18:33:55 +0900

    add new categories @pxolly and vkontakte

--- src/pxolly/types/categories.rs
+++ src/pxolly/types/categories.rs
@@ -1,1 +1,18 @@
+use crate::pxolly::api::PxollyAPI;
+use crate::pxolly::types::categories::callback::CallbackMethods;
+
 pub mod callback;
+
+pub struct Categories {
+    api_client: PxollyAPI
+}
+
+impl Categories {
+    pub fn new(api_client: PxollyAPI) -> Self {
+        Self { api_client }
+    }
+    
+    pub fn callback(&self) -> CallbackMethods {
+        CallbackMethods::new(self.api_client.clone())
+    }
+}
--- src/vkontakte/types/categories.rs
+++ src/vkontakte/types/categories.rs
@@ -1,6 +1,27 @@
+use crate::vkontakte::api::VKontakteAPI;
+use crate::vkontakte::errors::VKontakteError;
+
 pub mod messages;
 pub mod photos;
 
 pub struct Categories {
+    api_client: VKontakteAPI,
+}
+
+impl Categories {
+    pub fn new(api_client: VKontakteAPI) -> Self {
+        Self { api_client }
+    }
     
+    pub fn messages(&self) -> MessagesMethods {
+        MessagesMethods::new(self.api_client.clone())
+    }
+    
+    pub fn photos(&self) -> PhotosMethods {
+        PhotosMethods::new(self.api_client.clone())
+    }
+    
+    pub async fn execute(&self, params: ExecuteParams) -> Result<ExecuteResponse, VKontakteError> {
+        self.api_client.api_request("execute", params).await
+    }
 }

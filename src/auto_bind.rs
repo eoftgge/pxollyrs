82c3b295918b8b4058d15aaf9894866e394db2c6
consteremnaresinf <sandaar.ykt@gmail.com>
2024-09-04 23:58:05 +0900

    rewrote check error auto_bind

--- src/auto_bind.rs
+++ src/auto_bind.rs
@@ -1,9 +1,11 @@
 use crate::pxolly::api::PxollyAPI;
 use crate::pxolly::types::categories::Categories;
 use crate::pxolly::types::params::EditSettingsParams;
+use crate::pxolly::types::responses::callback::EditSettingsResponse;
 
 pub async fn auto_bind(api: PxollyAPI, is_bind: bool, secret_key: String, host: String, pxolly_url: String) {
     if is_bind || host == pxolly_url {
+        log::info!("auto_bind won't run.");
         return;
     }
 
@@ -15,10 +17,10 @@ pub async fn auto_bind(api: PxollyAPI, is_bind: bool, secret_key: String, host: 
             is_hidden: false,
         })
         .await;
-
-    if let Ok(response) = response {
-        log::info!("Result bind webhook: {:?}", response)
-    } else if let Err(error) = response {
-        log::error!("Failed bind webhook: {:?}", error)
+    
+    match response {
+        Ok(EditSettingsResponse { state: 200, .. }) => log::info!("Successfully bind webhook"),
+        Ok(err) => log::error!("Failed bind webhook: {:?}", err),
+        Err(err) => log::error!("Failed calling method @pxolly: {:?}", err),
     }
 }

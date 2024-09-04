88bbde7cc060f4c09699bd44620e0e82ca32ce9b
consteremnaresinf <sandaar.ykt@gmail.com>
2024-09-05 01:45:29 +0900

    added a crate serde-qs for fixing error

--- src/pxolly/errors.rs
+++ src/pxolly/errors.rs
@@ -4,7 +4,7 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum PxollyError {
     #[error("HTTP: {0}")]
-    Http(#[from] reqwest::Error),
+    HTTP(#[from] reqwest::Error),
     #[error("JSON: {0}")]
     Json(#[from] serde_json::Error),
     #[error("API: {0:?}")]
@@ -13,4 +13,6 @@ pub enum PxollyError {
     RmpEncoded(#[from] rmp_serde::encode::Error),
     #[error("Decoded: {0}")]
     RmpDecoded(#[from] rmp_serde::decode::Error),
+    #[error("QS: {0}")]
+    QS(#[from] serde_qs::Error),
 }
--- src/vkontakte/errors.rs
+++ src/vkontakte/errors.rs
@@ -4,9 +4,11 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum VKontakteError {
     #[error("HTTP: {0}")]
-    Http(#[from] reqwest::Error),
+    HTTP(#[from] reqwest::Error),
     #[error("API: {0:?}")]
     API(VKontakteAPIError),
     #[error("JSON: {0}")]
     Json(#[from] serde_json::Error),
+    #[error("QS: {0}")]
+    QS(#[from] serde_qs::Error),
 }

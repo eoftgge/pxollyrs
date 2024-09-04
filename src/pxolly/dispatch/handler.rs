c9f9e42819c9f2432f2b9f1d00de9098d7aadb1b
consteremnaresinf <sandaar.ykt@gmail.com>
2024-09-04 22:36:52 +0900

    added a new trait for associated type handler

--- src/pxolly/dispatch/handler.rs
+++ src/pxolly/dispatch/handler.rs
@@ -1,11 +1,12 @@
 use crate::pxolly::types::responses::errors::PxollyWebhookError;
 use crate::pxolly::types::responses::webhook::PxollyWebhookResponse;
 use std::future::Future;
+use serde::de::DeserializeOwned;
 use crate::pxolly::types::events::event_type::EventType;
 
 pub trait Handler: Send + Sync + 'static {
     const EVENT_TYPE: EventType;
-    type EventObject;
+    type EventObject: DeserializeOwned;
 
     fn handle(
         &self,
